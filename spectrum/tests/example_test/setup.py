65114a8e585a5c2f5379258b30d79a6dbdd17884
Cristian Camargo <ccf@cdone.com.br>
2024-06-09 23:58:17 -0300

    Add a extra example to the setup of some new callbacks that can be added to allow extra functionalities and behavior in the end of tests as a extra step and something that will receive the test resumes for example.

--- spectrum/tests/example_test/setup.py
+++ spectrum/tests/example_test/setup.py
@@ -2,4 +2,21 @@ def main():
     print("Setting up the environment for example_test")
     # Setup logic here
 
+# -> This functill will be called when the test end, and summary of the test will be in the kwargs
+def end_callback(**kwargs):
+    """
+    kwargs contain:
+    - test duration
+    - test that passed
+    - test that failed
+    - events complete
+    - events missing to be complete
+    """
+    pass
 
+# -> This function will handle a extra verification step in the end of the others that you migh want to add
+def extra_step():
+    """
+    Return only accept boolen, none will be false and false fail the test!
+    """
+    pass
